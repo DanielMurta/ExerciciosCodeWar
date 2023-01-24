758ffafb052ecc15e69cae56484434cd4a1ff47c
Daniel Silveira <murtdaniel@gmail.com>
2023-01-24 12:08:55 -0300

    .

---
+++ JavaScript/Sum_Arrays.js
@@ -0,0 +1,13 @@
+function sum (numbers) {
+    let sum = 0
+    for (let c = 0; c < numbers.length; c++) {
+        sum += numbers[c]
+    }
+
+    return sum
+}
+
+sum([1, 5.2, 4, 0, -1])
+
+// Outra Solução - Another Solution
+// return numbers.reduce((a, b) => a + b, 0);
