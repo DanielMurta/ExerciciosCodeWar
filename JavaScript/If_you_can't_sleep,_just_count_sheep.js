10e109be12bed398b0b33ced4ed9d43f4a3ea6ce
Daniel Silveira <murtdaniel@gmail.com>
2023-02-15 15:24:25 -0300

    [Novos exercícios]

--- JavaScript/If_you_can't_sleep,_just_count_sheep.js
+++ JavaScript/If_you_can't_sleep,_just_count_sheep.js
@@ -1,3 +1,21 @@
 var countSheep = function (num){
-    //your code here
-  }
+    let count = []
+    if (num < 1){
+        return ''
+    } else {
+        for (let c = 1;c < num + 1; c++){
+            count.push(`${c} sheep...`)
+        }
+    }
+
+    return string = count.join('')
+    
+}
+
+// test
+console.log(countSheep(5))
+
+// Another Solution
+//let str = "";
+//  for(let i = 1; i <= num; i++) { str+= `${i} sheep...`; }
+//  return str;
---
+++ JavaScript/Switch_it_Up!.js
@@ -0,0 +1,7 @@
+function switchItUp (number){
+    let numbers = ['Zero', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine']
+    return numbers[number]
+}
+
+// Test
+console.log(switchItUp(0))
